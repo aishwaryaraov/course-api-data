f0f11a0832e4ebc8c52e52fb95278bcc823c46e3
aishwaryaraov <aishwarya.rao91@gmail.com>
2020-02-25 21:54:14 -0600

    A sample springboot app

---
+++ src/main/java/com/io/java/springstarter/topic/TopicRepository.java
@@ -0,0 +1,8 @@
+package com.io.java.springstarter.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String>{
+	
+	  
+}
---
+++ src/test/java/com/io/java/springstarter/CourseApiDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.java.springstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
